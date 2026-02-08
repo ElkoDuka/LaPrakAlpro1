f012c3d9f2c34073e615d63c4a13ca7b8736ae98
ElkoDuka <eliqohen@gmail.com>
2026-02-08 18:39:46 +0700

    Add gold investment profit calculation script

---
+++ emas.py
@@ -0,0 +1,29 @@
+Jumlah_emas = 25
+Harga_emas_awal = 650000
+Harga_emas_saat_ini = 685000
+
+Invest_awal = Harga_emas_awal * Jumlah_emas
+Nilai_emas_saat_ini = Harga_emas_saat_ini * Jumlah_emas
+Nilai_profit_awal = Nilai_emas_saat_ini - Invest_awal
+Persen_nilai_awal = (Nilai_profit_awal / Invest_awal) * 100
+
+print("Profit dari pembelian awal:")
+print("Rupiah: Rp", Nilai_profit_awal)
+print("Persen:", round(Persen_nilai_awal, 2), "%")
+
+Tambahan_emas = 15
+Harga_tambah_emas = Harga_emas_saat_ini
+Invest_tambahan = Harga_tambah_emas * Tambahan_emas
+
+Total_emas = Jumlah_emas + Tambahan_emas
+Invest_total = Invest_awal + Invest_tambahan
+
+Harga_emas_akhir = 715000
+Nilai_emas_ahkir = Total_emas * Harga_emas_akhir
+
+Nilai_profit_total = Nilai_emas_ahkir - Invest_total
+Persen_nilai_total = (Nilai_profit_total / Invest_total) * 100
+
+print("\nProfit kenaikan harga dan penambahan emas:")
+print("Rupiah: Rp", Nilai_profit_total)
+print("Persen", round(Persen_nilai_total, 2), "%")
